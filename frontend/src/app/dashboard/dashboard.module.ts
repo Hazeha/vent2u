97a43fad911b13ddf0bfd50f035a68f386b4fa99
Hazeha <krantja@gmail.com>
2020-11-14 20:44:28 +0100

    yea

--- frontend/src/app/dashboard/dashboard.module.ts
+++ frontend/src/app/dashboard/dashboard.module.ts
@@ -8,30 +8,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard.component';
 
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-
-
-
 import { HeaderComponent } from '../_components/Header/header.component';
-import { MatMenuModule } from '@angular/material/menu';
 import { NavigationComponent } from '../_components/Navigation/navigation.component';
 
+import { TemperatureComponent } from './components/temperature/temperature.component';
+import { CheckinComponent } from './components/checkin/checkin.component';
+import { LightsComponent } from './components/lights/lights.component';
+import { SettingsComponent } from './components/settings/settings.component';
+
+
+import { OTempComponent } from './components/temperature/OutsideTemp/otemp.component';
+import { OTempModule } from './components/temperature/OutsideTemp/otemp.module';
 // Pipelines
 import { CelsiusPipe } from '../celsius.pipe';
+
+
+// FUCK THIS!
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatSliderModule} from '@angular/material/slider';
-import { LightsComponent } from './components/lights/lights.component';
-import { CheckinComponent } from './components/checkin/checkin.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
-import { SettingsComponent } from './components/settings/settings.component';
 import {MatButtonModule} from '@angular/material/button';
 
 import { MaterialModule } from '../_material/material.module';
+
+
 
 
 @NgModule({
@@ -40,15 +49,20 @@ import { MaterialModule } from '../_material/material.module';
     // Pages
     HeaderComponent,
     NavigationComponent,
-    CelsiusPipe,
-    LightsComponent,
     CheckinComponent,
+    TemperatureComponent,
+    LightsComponent,
     SettingsComponent,
+
+    OTempComponent,
+    CelsiusPipe,
 
   ],
   imports: [
     CommonModule,
     RouterModule,
+    FormsModule,
+    OTempModule,
 
     MatIconModule,
     MatToolbarModule,
